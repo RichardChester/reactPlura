9d9720e13a9f55b61bc6f91633c15cea5a85a261
RichardChester <richard.chester93@gmail.com>
2018-09-23 21:55:48 +0100

    a work in progress

---
+++ src/components/Home.js
@@ -0,0 +1,16 @@
+"use strict";
+
+var react = require('react')
+
+
+var home = react.createClass({
+    render() {
+        return (
+            <div className='jumbotron'></div>
+                <H1>"Plreaight admin"</H1>
+
+         );
+    }
+});
+
+module.exports = home;
---
+++ src/main.js
@@ -0,0 +1,8 @@
+$=jquery=require('jquery');
+
+
+
+
+var app = console.log('hello world brow-fi');
+
+module.exports=app;
